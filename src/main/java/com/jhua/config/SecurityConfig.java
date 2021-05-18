6fc19c81930d8ae6a5ea2bf0b844c02a21c5735c
wb.xiejiehua <wb.xiejiehua@mesg.corp.netease.com>
2021-05-18 19:20:11 +0800

    2021-05-18

--- src/main/java/com/jhua/config/SecurityConfig.java
+++ src/main/java/com/jhua/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.servlet.http.HttpSession;
 
@@ -20,20 +21,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/").permitAll()
                 .antMatchers("/R/**").hasRole("R")
                 .antMatchers("/SR/**").hasRole("SR")
-                .antMatchers("/SSR/**").hasRole("SSR");
+                .antMatchers("/SSR/**").hasRole("SSR")
+                .antMatchers("/level1/**").hasRole("R")
+                .antMatchers("/level2/**").hasRole("SR")
+                .antMatchers("/level3/**").hasRole("SSR");
 
         http.formLogin();
+
+//        http.csrf().disable();//关闭csrf功能
+        //注销
+        http.logout().logoutSuccessUrl("/");
     }
 
     //认证
-
+    //密码编码：PasswordEncoder
+    //在Spring Security 5.0+ 新增了许多加密方法
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("jhua").password("123456").roles("SSR", "SR", "R")
+
+        // 下面这些数据正常应该从数据库中读取
+        auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("jhua").password(new BCryptPasswordEncoder().encode("1")).roles("SSR", "SR", "R")
                 .and()
-                .withUser("manR").password("123").roles("R")
+                .withUser("manR").password(new BCryptPasswordEncoder().encode("1")).roles("R")
                 .and()
-                .withUser("manSR").password("123456").roles("SR", "R");
+                .withUser("manSR").password(new BCryptPasswordEncoder().encode("1")).roles("SR", "R");
     }
 }
---
+++ src/main/java/com/jhua/controller/LoginController.java
@@ -0,0 +1,43 @@
+package com.jhua.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/")
+    public String index() {
+        System.out.println("index");
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        System.out.println("login");
+        return "index";
+    }
+
+    @RequestMapping({"/toLogin"})
+    public String afterLogin() {
+        System.out.println("toLogin");
+        return "views/login";
+    }
+
+    @RequestMapping("/level1/{id}")
+    public String r(@PathVariable("id") int id) {
+        return "views/level1/"+id;
+    }
+
+    @RequestMapping("/level2/{id}")
+    public String sr(@PathVariable("id") int id) {
+        return "views/level2/"+id;
+    }
+
+    @RequestMapping("/level3/{id}")
+    public String ssr(@PathVariable("id") int id) {
+        return "views/level3/"+id;
+    }
+
+}
